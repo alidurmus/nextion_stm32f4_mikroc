a5f3720d1fd607dd582236550c7e0a16fc24ea1d
alidurmus <alidurmus1981@hotmail.com>
2016-12-25 21:08:26 +0200

    upload project

--- examples/number/mikroc/uartx.asm
+++ examples/number/mikroc/uartx.asm
@@ -1,128 +1,129 @@
+_hextostr:
+;uartx.c,33 :: 		void hextostr(char eleman)
+; eleman start address is: 0 (R0)
+SUB	SP, SP, #4
+UXTB	R2, R0
+; eleman end address is: 0 (R0)
+; eleman start address is: 8 (R2)
+;uartx.c,36 :: 		sol=(eleman>>4)&0x0F; //0000 0001 dört basamak kaydýr 0001 0000 ve 0000 11111
+LSRS	R1, R2, #4
+UXTB	R1, R1
+AND	R1, R1, #15
+UXTB	R1, R1
+; sol start address is: 0 (R0)
+UXTB	R0, R1
+;uartx.c,37 :: 		if(sol>=10)
+CMP	R1, #10
+IT	CC
+BCC	L_hextostr0
+;uartx.c,39 :: 		sol+=55;
+ADDW	R1, R0, #55
+UXTB	R0, R1
+;uartx.c,40 :: 		}
+IT	AL
+BAL	L_hextostr1
+L_hextostr0:
+;uartx.c,42 :: 		sol+=48; //  0001 => 1 + 48
+ADDW	R1, R0, #48
+UXTB	R0, R1
+; sol end address is: 0 (R0)
+L_hextostr1:
+;uartx.c,44 :: 		sag=eleman&0x0F;  // 0001 0000 ve 0000 1111
+; sol start address is: 0 (R0)
+AND	R1, R2, #15
+UXTB	R1, R1
+; eleman end address is: 8 (R2)
+; sag start address is: 8 (R2)
+UXTB	R2, R1
+;uartx.c,46 :: 		if(sag>=10)
+CMP	R1, #10
+IT	CC
+BCC	L_hextostr2
+;uartx.c,48 :: 		sag+=55;
+ADDW	R1, R2, #55
+; sag end address is: 8 (R2)
+; sag start address is: 12 (R3)
+UXTB	R3, R1
+;uartx.c,49 :: 		}
+; sag end address is: 12 (R3)
+IT	AL
+BAL	L_hextostr3
+L_hextostr2:
+;uartx.c,51 :: 		sag+=48;
+; sag start address is: 8 (R2)
+ADDW	R1, R2, #48
+; sag end address is: 8 (R2)
+; sag start address is: 12 (R3)
+UXTB	R3, R1
+; sag end address is: 12 (R3)
+L_hextostr3:
+;uartx.c,52 :: 		txt[0]='0';
+; sag start address is: 12 (R3)
+MOVS	R2, #48
+MOVW	R1, #lo_addr(_txt+0)
+MOVT	R1, #hi_addr(_txt+0)
+STRB	R2, [R1, #0]
+;uartx.c,53 :: 		txt[1]='x';
+MOVS	R2, #120
+MOVW	R1, #lo_addr(_txt+1)
+MOVT	R1, #hi_addr(_txt+1)
+STRB	R2, [R1, #0]
+;uartx.c,54 :: 		txt[2]=sol;
+MOVW	R1, #lo_addr(_txt+2)
+MOVT	R1, #hi_addr(_txt+2)
+STRB	R0, [R1, #0]
+; sol end address is: 0 (R0)
+;uartx.c,55 :: 		txt[3]=sag;
+MOVW	R1, #lo_addr(_txt+3)
+MOVT	R1, #hi_addr(_txt+3)
+STRB	R3, [R1, #0]
+; sag end address is: 12 (R3)
+;uartx.c,56 :: 		}
+L_end_hextostr:
+ADD	SP, SP, #4
+BX	LR
+; end of _hextostr
 _main:
-;uartx.c,12 :: 		void main()
-;uartx.c,15 :: 		&_GPIO_MODULE_USART2_PA23);
+;uartx.c,101 :: 		void main()
+SUB	SP, SP, #4
+;uartx.c,104 :: 		&_GPIO_MODULE_USART2_PA23);
 MOVW	R0, #lo_addr(__GPIO_MODULE_USART2_PA23+0)
 MOVT	R0, #hi_addr(__GPIO_MODULE_USART2_PA23+0)
 PUSH	(R0)
-;uartx.c,14 :: 		UART2_Init_Advanced(57600, _UART_8_BIT_DATA, _UART_NOPARITY, _UART_ONE_STOPBIT,
+;uartx.c,103 :: 		UART2_Init_Advanced(57600, _UART_8_BIT_DATA, _UART_NOPARITY, _UART_ONE_STOPBIT,
 MOVW	R3, #0
 MOVW	R2, #0
 MOVW	R1, #0
 MOVW	R0, #57600
-;uartx.c,15 :: 		&_GPIO_MODULE_USART2_PA23);
+;uartx.c,104 :: 		&_GPIO_MODULE_USART2_PA23);
 BL	_UART2_Init_Advanced+0
 ADD	SP, SP, #4
-;uartx.c,18 :: 		&_GPIO_MODULE_USART3_PD89);
-MOVW	R0, #lo_addr(__GPIO_MODULE_USART3_PD89+0)
-MOVT	R0, #hi_addr(__GPIO_MODULE_USART3_PD89+0)
-PUSH	(R0)
-;uartx.c,17 :: 		UART3_Init_Advanced(57600, _UART_8_BIT_DATA, _UART_NOPARITY, _UART_ONE_STOPBIT,
-MOVW	R3, #0
-MOVW	R2, #0
-MOVW	R1, #0
-MOVW	R0, #57600
-;uartx.c,18 :: 		&_GPIO_MODULE_USART3_PD89);
-BL	_UART3_Init_Advanced+0
-ADD	SP, SP, #4
-;uartx.c,20 :: 		Delay_ms(200);
+;uartx.c,106 :: 		Delay_ms(200);
 MOVW	R7, #27219
 MOVT	R7, #142
 NOP
 NOP
-L_main0:
+L_main4:
 SUBS	R7, R7, #1
-BNE	L_main0
+BNE	L_main4
 NOP
 NOP
 NOP
 NOP
-;uartx.c,21 :: 		UART2_Write_Text("ali"); //
-MOVW	R0, #lo_addr(?lstr1_uartx+0)
-MOVT	R0, #hi_addr(?lstr1_uartx+0)
-BL	_UART2_Write_Text+0
-;uartx.c,23 :: 		EnableInterrupts(); // enable core interrupts
+;uartx.c,108 :: 		EnableInterrupts();// enable interrupts.
 BL	_EnableInterrupts+0
-;uartx.c,26 :: 		USART2_CR1bits.RXNEIE = 1; // enable uart rx interrupt
+;uartx.c,109 :: 		USART2_CR1bits.RXNEIE = 1; // enable uart rx interrupt
 MOVS	R1, #1
 SXTB	R1, R1
 MOVW	R0, #lo_addr(USART2_CR1bits+0)
 MOVT	R0, #hi_addr(USART2_CR1bits+0)
 STR	R1, [R0, #0]
-;uartx.c,27 :: 		NVIC_IntEnable(IVT_INT_USART2); // enable interrupt vector
+;uartx.c,110 :: 		NVIC_IntEnable(IVT_INT_USART2); // enable interrupt vector
 MOVW	R0, #54
 BL	_NVIC_IntEnable+0
-;uartx.c,30 :: 		while(1)                                 //Sonsuz döngü.
-L_main2:
-;uartx.c,43 :: 		if(i==6)   // Eðer UART3'e veri gelmiþ ise
-MOVW	R0, #lo_addr(_i+0)
-MOVT	R0, #hi_addr(_i+0)
-LDRSH	R0, [R0, #0]
-CMP	R0, #6
-IT	NE
-BNE	L_main4
-;uartx.c,45 :: 		Delay_ms(200);
-MOVW	R7, #27219
-MOVT	R7, #142
-NOP
-NOP
-L_main5:
-SUBS	R7, R7, #1
-BNE	L_main5
-NOP
-NOP
-NOP
-NOP
-;uartx.c,46 :: 		UART2_Write(rx_array[0]);
-MOVW	R0, #lo_addr(_rx_array+0)
-MOVT	R0, #hi_addr(_rx_array+0)
-LDRB	R0, [R0, #0]
-BL	_UART2_Write+0
-;uartx.c,47 :: 		UART2_Write(rx_array[1]);
-MOVW	R0, #lo_addr(_rx_array+1)
-MOVT	R0, #hi_addr(_rx_array+1)
-LDRB	R0, [R0, #0]
-BL	_UART2_Write+0
-;uartx.c,48 :: 		UART2_Write(rx_array[2]);
-MOVW	R0, #lo_addr(_rx_array+2)
-MOVT	R0, #hi_addr(_rx_array+2)
-LDRB	R0, [R0, #0]
-BL	_UART2_Write+0
-;uartx.c,49 :: 		UART2_Write(rx_array[3]);
-MOVW	R0, #lo_addr(_rx_array+3)
-MOVT	R0, #hi_addr(_rx_array+3)
-LDRB	R0, [R0, #0]
-BL	_UART2_Write+0
-;uartx.c,50 :: 		UART2_Write(rx_array[4]);
-MOVW	R0, #lo_addr(_rx_array+4)
-MOVT	R0, #hi_addr(_rx_array+4)
-LDRB	R0, [R0, #0]
-BL	_UART2_Write+0
-;uartx.c,51 :: 		UART2_Write(rx_array[5]);
-MOVW	R0, #lo_addr(_rx_array+5)
-MOVT	R0, #hi_addr(_rx_array+5)
-LDRB	R0, [R0, #0]
-BL	_UART2_Write+0
-;uartx.c,52 :: 		UART2_Write(rx_array[6]);
-MOVW	R0, #lo_addr(_rx_array+6)
-MOVT	R0, #hi_addr(_rx_array+6)
-LDRB	R0, [R0, #0]
-BL	_UART2_Write+0
-;uartx.c,53 :: 		i=0;
-MOVS	R1, #0
-SXTH	R1, R1
-MOVW	R0, #lo_addr(_i+0)
-MOVT	R0, #hi_addr(_i+0)
-STRH	R1, [R0, #0]
-;uartx.c,54 :: 		}
-L_main4:
-;uartx.c,57 :: 		if(j==8)   // Eðer UART3'e veri gelmiþ ise
-MOVW	R0, #lo_addr(_j+0)
-MOVT	R0, #hi_addr(_j+0)
-LDRSH	R0, [R0, #0]
-CMP	R0, #8
-IT	NE
-BNE	L_main7
-;uartx.c,59 :: 		Delay_ms(200);
+;uartx.c,114 :: 		while(1) //Sonsuz döngü.
+L_main6:
+;uartx.c,124 :: 		Delay_ms(200);
 MOVW	R7, #27219
 MOVT	R7, #142
 NOP
@@ -134,114 +135,256 @@ NOP
 NOP
 NOP
 NOP
-;uartx.c,60 :: 		UART3_Write(rx_array2[0]);
-MOVW	R0, #lo_addr(_rx_array2+0)
-MOVT	R0, #hi_addr(_rx_array2+0)
+;uartx.c,127 :: 		if (rx_array[0] == 0x65&& i==6){
+MOVW	R0, #lo_addr(_rx_array+0)
+MOVT	R0, #hi_addr(_rx_array+0)
 LDRB	R0, [R0, #0]
-BL	_UART3_Write+0
-;uartx.c,61 :: 		UART3_Write(rx_array2[1]);
-MOVW	R0, #lo_addr(_rx_array2+1)
-MOVT	R0, #hi_addr(_rx_array2+1)
+CMP	R0, #101
+IT	NE
+BNE	L__main34
+MOVW	R0, #lo_addr(_i+0)
+MOVT	R0, #hi_addr(_i+0)
+LDRSH	R0, [R0, #0]
+CMP	R0, #6
+IT	NE
+BNE	L__main33
+L__main32:
+;uartx.c,128 :: 		if (rx_array[1] == 0x00){
+MOVW	R0, #lo_addr(_rx_array+1)
+MOVT	R0, #hi_addr(_rx_array+1)
 LDRB	R0, [R0, #0]
-BL	_UART3_Write+0
-;uartx.c,62 :: 		UART3_Write(rx_array2[2]);
-MOVW	R0, #lo_addr(_rx_array2+2)
-MOVT	R0, #hi_addr(_rx_array2+2)
+CMP	R0, #0
+IT	NE
+BNE	L_main13
+;uartx.c,129 :: 		if (rx_array[2] == 0x01){
+MOVW	R0, #lo_addr(_rx_array+2)
+MOVT	R0, #hi_addr(_rx_array+2)
 LDRB	R0, [R0, #0]
-BL	_UART3_Write+0
-;uartx.c,63 :: 		UART3_Write(rx_array2[3]);
-MOVW	R0, #lo_addr(_rx_array2+3)
-MOVT	R0, #hi_addr(_rx_array2+3)
+CMP	R0, #1
+IT	NE
+BNE	L_main14
+;uartx.c,130 :: 		if (rx_array[3] == 0x00){
+MOVW	R0, #lo_addr(_rx_array+3)
+MOVT	R0, #hi_addr(_rx_array+3)
 LDRB	R0, [R0, #0]
-BL	_UART3_Write+0
-;uartx.c,64 :: 		UART3_Write(rx_array2[4]);
-MOVW	R0, #lo_addr(_rx_array2+4)
-MOVT	R0, #hi_addr(_rx_array2+4)
+CMP	R0, #0
+IT	NE
+BNE	L_main15
+;uartx.c,131 :: 		if (rx_array[4] == 0xFF){
+MOVW	R0, #lo_addr(_rx_array+4)
+MOVT	R0, #hi_addr(_rx_array+4)
 LDRB	R0, [R0, #0]
-BL	_UART3_Write+0
-;uartx.c,65 :: 		UART3_Write(rx_array2[5]);
-MOVW	R0, #lo_addr(_rx_array2+5)
-MOVT	R0, #hi_addr(_rx_array2+5)
+CMP	R0, #255
+IT	NE
+BNE	L_main16
+;uartx.c,132 :: 		if (rx_array[5] == 0xFF){
+MOVW	R0, #lo_addr(_rx_array+5)
+MOVT	R0, #hi_addr(_rx_array+5)
 LDRB	R0, [R0, #0]
-BL	_UART3_Write+0
-;uartx.c,66 :: 		UART3_Write(rx_array2[6]);
-MOVW	R0, #lo_addr(_rx_array2+6)
-MOVT	R0, #hi_addr(_rx_array2+6)
+CMP	R0, #255
+IT	NE
+BNE	L_main17
+;uartx.c,133 :: 		if (rx_array[6] == 0xFF){
+MOVW	R0, #lo_addr(_rx_array+6)
+MOVT	R0, #hi_addr(_rx_array+6)
 LDRB	R0, [R0, #0]
-BL	_UART3_Write+0
-;uartx.c,67 :: 		UART3_Write(rx_array2[7]);
-MOVW	R0, #lo_addr(_rx_array2+7)
-MOVT	R0, #hi_addr(_rx_array2+7)
-LDRB	R0, [R0, #0]
-BL	_UART3_Write+0
-;uartx.c,68 :: 		UART3_Write(rx_array2[8]);
-MOVW	R0, #lo_addr(_rx_array2+8)
-MOVT	R0, #hi_addr(_rx_array2+8)
-LDRB	R0, [R0, #0]
-BL	_UART3_Write+0
-;uartx.c,69 :: 		UART3_Write(0xFF);
+CMP	R0, #255
+IT	NE
+BNE	L_main18
+;uartx.c,134 :: 		USART2_CR1bits.RXNEIE = 0;
+MOVS	R1, #0
+SXTB	R1, R1
+MOVW	R0, #lo_addr(USART2_CR1bits+0)
+MOVT	R0, #hi_addr(USART2_CR1bits+0)
+STR	R1, [R0, #0]
+;uartx.c,135 :: 		UART2_Write_Text("n0.val=44"); //
+MOVW	R0, #lo_addr(?lstr1_uartx+0)
+MOVT	R0, #hi_addr(?lstr1_uartx+0)
+BL	_UART2_Write_Text+0
+;uartx.c,136 :: 		UART2_Write(0xFF);
 MOVS	R0, #255
-BL	_UART3_Write+0
-;uartx.c,70 :: 		UART3_Write(0xFF);
+BL	_UART2_Write+0
+;uartx.c,137 :: 		UART2_Write(0xFF);
 MOVS	R0, #255
-BL	_UART3_Write+0
-;uartx.c,71 :: 		UART3_Write(0xFF);
+BL	_UART2_Write+0
+;uartx.c,138 :: 		UART2_Write(0xFF);
 MOVS	R0, #255
-BL	_UART3_Write+0
-;uartx.c,72 :: 		j=0;
+BL	_UART2_Write+0
+;uartx.c,139 :: 		USART2_CR1bits.RXNEIE = 1;
+MOVS	R1, #1
+SXTB	R1, R1
+MOVW	R0, #lo_addr(USART2_CR1bits+0)
+MOVT	R0, #hi_addr(USART2_CR1bits+0)
+STR	R1, [R0, #0]
+;uartx.c,140 :: 		i=0;
 MOVS	R1, #0
 SXTH	R1, R1
-MOVW	R0, #lo_addr(_j+0)
-MOVT	R0, #hi_addr(_j+0)
+MOVW	R0, #lo_addr(_i+0)
+MOVT	R0, #hi_addr(_i+0)
 STRH	R1, [R0, #0]
-;uartx.c,73 :: 		}
-L_main7:
-;uartx.c,76 :: 		} //while
+;uartx.c,141 :: 		}
+L_main18:
+;uartx.c,142 :: 		}
+L_main17:
+;uartx.c,143 :: 		}
+L_main16:
+;uartx.c,144 :: 		}
+L_main15:
+;uartx.c,145 :: 		}
+L_main14:
+;uartx.c,146 :: 		}
+L_main13:
+;uartx.c,127 :: 		if (rx_array[0] == 0x65&& i==6){
+L__main34:
+L__main33:
+;uartx.c,149 :: 		if (rx_array[0] == 0x65&& i==6){
+MOVW	R0, #lo_addr(_rx_array+0)
+MOVT	R0, #hi_addr(_rx_array+0)
+LDRB	R0, [R0, #0]
+CMP	R0, #101
+IT	NE
+BNE	L__main36
+MOVW	R0, #lo_addr(_i+0)
+MOVT	R0, #hi_addr(_i+0)
+LDRSH	R0, [R0, #0]
+CMP	R0, #6
+IT	NE
+BNE	L__main35
+L__main31:
+;uartx.c,150 :: 		if (rx_array[1] == 0x00){
+MOVW	R0, #lo_addr(_rx_array+1)
+MOVT	R0, #hi_addr(_rx_array+1)
+LDRB	R0, [R0, #0]
+CMP	R0, #0
+IT	NE
+BNE	L_main22
+;uartx.c,151 :: 		if (rx_array[2] == 0x02){
+MOVW	R0, #lo_addr(_rx_array+2)
+MOVT	R0, #hi_addr(_rx_array+2)
+LDRB	R0, [R0, #0]
+CMP	R0, #2
+IT	NE
+BNE	L_main23
+;uartx.c,152 :: 		if (rx_array[3] == 0x00){
+MOVW	R0, #lo_addr(_rx_array+3)
+MOVT	R0, #hi_addr(_rx_array+3)
+LDRB	R0, [R0, #0]
+CMP	R0, #0
+IT	NE
+BNE	L_main24
+;uartx.c,153 :: 		if (rx_array[4] == 0xFF){
+MOVW	R0, #lo_addr(_rx_array+4)
+MOVT	R0, #hi_addr(_rx_array+4)
+LDRB	R0, [R0, #0]
+CMP	R0, #255
+IT	NE
+BNE	L_main25
+;uartx.c,154 :: 		if (rx_array[5] == 0xFF){
+MOVW	R0, #lo_addr(_rx_array+5)
+MOVT	R0, #hi_addr(_rx_array+5)
+LDRB	R0, [R0, #0]
+CMP	R0, #255
+IT	NE
+BNE	L_main26
+;uartx.c,155 :: 		if (rx_array[6] == 0xFF){
+MOVW	R0, #lo_addr(_rx_array+6)
+MOVT	R0, #hi_addr(_rx_array+6)
+LDRB	R0, [R0, #0]
+CMP	R0, #255
+IT	NE
+BNE	L_main27
+;uartx.c,156 :: 		USART2_CR1bits.RXNEIE = 0;
+MOVS	R1, #0
+SXTB	R1, R1
+MOVW	R0, #lo_addr(USART2_CR1bits+0)
+MOVT	R0, #hi_addr(USART2_CR1bits+0)
+STR	R1, [R0, #0]
+;uartx.c,157 :: 		UART2_Write_Text("n0.val=55"); //
+MOVW	R0, #lo_addr(?lstr2_uartx+0)
+MOVT	R0, #hi_addr(?lstr2_uartx+0)
+BL	_UART2_Write_Text+0
+;uartx.c,158 :: 		UART2_Write(0xFF);
+MOVS	R0, #255
+BL	_UART2_Write+0
+;uartx.c,159 :: 		UART2_Write(0xFF);
+MOVS	R0, #255
+BL	_UART2_Write+0
+;uartx.c,160 :: 		UART2_Write(0xFF);
+MOVS	R0, #255
+BL	_UART2_Write+0
+;uartx.c,161 :: 		USART2_CR1bits.RXNEIE = 1;
+MOVS	R1, #1
+SXTB	R1, R1
+MOVW	R0, #lo_addr(USART2_CR1bits+0)
+MOVT	R0, #hi_addr(USART2_CR1bits+0)
+STR	R1, [R0, #0]
+;uartx.c,162 :: 		i=0;
+MOVS	R1, #0
+SXTH	R1, R1
+MOVW	R0, #lo_addr(_i+0)
+MOVT	R0, #hi_addr(_i+0)
+STRH	R1, [R0, #0]
+;uartx.c,163 :: 		}
+L_main27:
+;uartx.c,164 :: 		}
+L_main26:
+;uartx.c,165 :: 		}
+L_main25:
+;uartx.c,166 :: 		}
+L_main24:
+;uartx.c,167 :: 		}
+L_main23:
+;uartx.c,168 :: 		}
+L_main22:
+;uartx.c,149 :: 		if (rx_array[0] == 0x65&& i==6){
+L__main36:
+L__main35:
+;uartx.c,173 :: 		}
 IT	AL
-BAL	L_main2
-;uartx.c,77 :: 		}
+BAL	L_main6
+;uartx.c,174 :: 		}
 L_end_main:
 L__main_end_loop:
 B	L__main_end_loop
 ; end of _main
 _interrupt:
-;uartx.c,90 :: 		void interrupt() iv IVT_INT_USART2 ics ICS_AUTO {
+;uartx.c,177 :: 		void interrupt() iv IVT_INT_USART2 ics ICS_AUTO {
 SUB	SP, SP, #12
 STR	LR, [SP, #0]
-;uartx.c,91 :: 		if(UART2_Data_Ready()&& j<=8) // If data is received
+;uartx.c,182 :: 		if(UART2_Data_Ready()&& i<=6) // If data is received
 BL	_UART2_Data_Ready+0
 CMP	R0, #0
 IT	EQ
-BEQ	L__interrupt15
-MOVW	R0, #lo_addr(_j+0)
-MOVT	R0, #hi_addr(_j+0)
+BEQ	L__interrupt39
+MOVW	R0, #lo_addr(_i+0)
+MOVT	R0, #hi_addr(_i+0)
 LDRSH	R0, [R0, #0]
-CMP	R0, #8
+CMP	R0, #6
 IT	GT
-BGT	L__interrupt14
-L__interrupt13:
-;uartx.c,93 :: 		rx_array2[j]=Uart2_Read();
-MOVW	R0, #lo_addr(_j+0)
-MOVT	R0, #hi_addr(_j+0)
+BGT	L__interrupt38
+L__interrupt37:
+;uartx.c,184 :: 		rx_array[i]=Uart2_Read();
+MOVW	R0, #lo_addr(_i+0)
+MOVT	R0, #hi_addr(_i+0)
 STR	R0, [SP, #8]
 LDRSH	R1, [R0, #0]
-MOVW	R0, #lo_addr(_rx_array2+0)
-MOVT	R0, #hi_addr(_rx_array2+0)
+MOVW	R0, #lo_addr(_rx_array+0)
+MOVT	R0, #hi_addr(_rx_array+0)
 ADDS	R0, R0, R1
 STR	R0, [SP, #4]
 BL	_UART2_Read+0
 LDR	R1, [SP, #4]
 STRB	R0, [R1, #0]
-;uartx.c,94 :: 		j++;
+;uartx.c,185 :: 		i++;
 LDR	R1, [SP, #8]
 MOV	R0, R1
 LDRSH	R0, [R0, #0]
 ADDS	R0, R0, #1
 STRH	R0, [R1, #0]
-;uartx.c,91 :: 		if(UART2_Data_Ready()&& j<=8) // If data is received
-L__interrupt15:
-L__interrupt14:
-;uartx.c,96 :: 		}
+;uartx.c,182 :: 		if(UART2_Data_Ready()&& i<=6) // If data is received
+L__interrupt39:
+L__interrupt38:
+;uartx.c,187 :: 		}
 L_end_interrupt:
 LDR	LR, [SP, #0]
 ADD	SP, SP, #12
--- examples/number/mikroc/uartx.cp
+++ examples/number/mikroc/uartx.cp
@@ -1,68 +1,113 @@
-#line 1 "B:/ELEKTRONÝK/deneme/uartxx_calisan/uartxx/uartx.c"
-#line 9 "B:/ELEKTRONÝK/deneme/uartxx_calisan/uartxx/uartx.c"
+#line 1 "B:/ELEKTRONÝK/deneme/number/uartx.c"
+#line 10 "B:/ELEKTRONÝK/deneme/number/uartx.c"
+char gelen;
+long page,component;
+char rx_buffer,rx_array[8];
 int i=0,j=0;
-char gelen,rx_array[10],rx_array2[10];
+char txt[4];
+#line 33 "B:/ELEKTRONÝK/deneme/number/uartx.c"
+void hextostr(char eleman)
+{
+ char sol,sag;
+ sol=(eleman>>4)&0x0F;
+ if(sol>=10)
+ {
+ sol+=55;
+ }
+ else
+ sol+=48;
 
+ sag=eleman&0x0F;
+
+ if(sag>=10)
+ {
+ sag+=55;
+ }
+ else
+ sag+=48;
+ txt[0]='0';
+ txt[1]='x';
+ txt[2]=sol;
+ txt[3]=sag;
+}
+#line 101 "B:/ELEKTRONÝK/deneme/number/uartx.c"
 void main()
 {
  UART2_Init_Advanced(57600, _UART_8_BIT_DATA, _UART_NOPARITY, _UART_ONE_STOPBIT,
  &_GPIO_MODULE_USART2_PA23);
 
- UART3_Init_Advanced(57600, _UART_8_BIT_DATA, _UART_NOPARITY, _UART_ONE_STOPBIT,
- &_GPIO_MODULE_USART3_PD89);
-
  Delay_ms(200);
- UART2_Write_Text("ali");
 
  EnableInterrupts();
-#line 26 "B:/ELEKTRONÝK/deneme/uartxx_calisan/uartxx/uartx.c"
  USART2_CR1bits.RXNEIE = 1;
  NVIC_IntEnable(IVT_INT_USART2);
 
 
-while(1)
+
+ while(1)
  {
-#line 43 "B:/ELEKTRONÝK/deneme/uartxx_calisan/uartxx/uartx.c"
- if(i==6)
- {
+#line 124 "B:/ELEKTRONÝK/deneme/number/uartx.c"
  Delay_ms(200);
- UART2_Write(rx_array[0]);
- UART2_Write(rx_array[1]);
- UART2_Write(rx_array[2]);
- UART2_Write(rx_array[3]);
- UART2_Write(rx_array[4]);
- UART2_Write(rx_array[5]);
- UART2_Write(rx_array[6]);
+
+
+ if (rx_array[0] == 0x65&& i==6){
+ if (rx_array[1] == 0x00){
+ if (rx_array[2] == 0x01){
+ if (rx_array[3] == 0x00){
+ if (rx_array[4] == 0xFF){
+ if (rx_array[5] == 0xFF){
+ if (rx_array[6] == 0xFF){
+ USART2_CR1bits.RXNEIE = 0;
+ UART2_Write_Text("n0.val=44");
+ UART2_Write(0xFF);
+ UART2_Write(0xFF);
+ UART2_Write(0xFF);
+ USART2_CR1bits.RXNEIE = 1;
  i=0;
  }
-
-
- if(j==8)
- {
- Delay_ms(200);
- UART3_Write(rx_array2[0]);
- UART3_Write(rx_array2[1]);
- UART3_Write(rx_array2[2]);
- UART3_Write(rx_array2[3]);
- UART3_Write(rx_array2[4]);
- UART3_Write(rx_array2[5]);
- UART3_Write(rx_array2[6]);
- UART3_Write(rx_array2[7]);
- UART3_Write(rx_array2[8]);
- UART3_Write(0xFF);
- UART3_Write(0xFF);
- UART3_Write(0xFF);
- j=0;
  }
+ }
+ }
+ }
+ }
+ }
+
+ if (rx_array[0] == 0x65&& i==6){
+ if (rx_array[1] == 0x00){
+ if (rx_array[2] == 0x02){
+ if (rx_array[3] == 0x00){
+ if (rx_array[4] == 0xFF){
+ if (rx_array[5] == 0xFF){
+ if (rx_array[6] == 0xFF){
+ USART2_CR1bits.RXNEIE = 0;
+ UART2_Write_Text("n0.val=55");
+ UART2_Write(0xFF);
+ UART2_Write(0xFF);
+ UART2_Write(0xFF);
+ USART2_CR1bits.RXNEIE = 1;
+ i=0;
+ }
+ }
+ }
+ }
+ }
+ }
+ }
+
 
 
  }
 }
-#line 90 "B:/ELEKTRONÝK/deneme/uartxx_calisan/uartxx/uartx.c"
-void interrupt() iv IVT_INT_USART2 ics ICS_AUTO {
- if(UART2_Data_Ready()&& j<=8)
+
+
+ void interrupt() iv IVT_INT_USART2 ics ICS_AUTO {
+
+
+
+
+ if(UART2_Data_Ready()&& i<=6)
  {
- rx_array2[j]=Uart2_Read();
- j++;
+ rx_array[i]=Uart2_Read();
+ i++;
  }
 }
